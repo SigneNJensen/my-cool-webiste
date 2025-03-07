c5641bdcd42c8bb791a94500866505ba39f61a6e
Signe <snj2740@gmail.com>
2025-03-07 21:12:24 +0100

    change fontsize of ballon

--- js/exercise2.js
+++ js/exercise2.js
@@ -1,6 +1,7 @@
 window.addEventListener("keyup", (event)=>{
     if (event.key === "ArrowUp") {
         let balloon = document.getElementById("balloon");
-        balloon.style.fontSize = "110%"
+
+        balloon.style.fontSize = "200%"
     }
 })

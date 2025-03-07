401266003fc0f4c4f116c2f6de750f6580505ca2
Signe <snj2740@gmail.com>
2025-03-07 22:21:34 +0100

    updated the exercise results

--- js/exercise2.js
+++ js/exercise2.js
@@ -1,7 +1,37 @@
-window.addEventListener("keyup", (event)=>{
-    if (event.key === "ArrowUp") {
+window.addEventListener("keydown", increase_fontsize)
+window.addEventListener("keydown", decrease_fontsize)
+
+function increase_fontsize(event) {
+    let balloon = document.getElementById("balloon");
+
+    //finds current size of ballon
+    let current_size = parseFloat(window.getComputedStyle(balloon).fontSize); //parseFloat changes the fontsize (which is string) to a float
+
+    if (current_size >= 150) {
+        balloon.innerText = "💥"; //changes the text
+        window.removeEventListener("keydown", increase_fontsize)
+        window.removeEventListener("keydown", decrease_fontsize)
+    }
+    else if (event.key === "ArrowUp") {
+        //prevents arrowup to scroll on page 
+        event.preventDefault();
+        
+        //new fond size of ballon
+        balloon.style.fontSize = (current_size * 1.1) + "px";
+    }
+}
+
+function decrease_fontsize(event) {
+    if (event.key === "ArrowDown") {
+        //prevents arrowup to scroll on page 
+        event.preventDefault();
+
         let balloon = document.getElementById("balloon");
 
-        balloon.style.fontSize = "200%"
+        //finds current size of ballon
+        let current_size = parseFloat(window.getComputedStyle(balloon).fontSize); //parseFloat changes the fontsize (which is string) to a float
+
+        //new fond size of ballon
+        balloon.style.fontSize = (current_size / 1.1) + "px";
     }
-})
+}
